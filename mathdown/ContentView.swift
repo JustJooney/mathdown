1390bbe892af370a2cf06935518c68d0fb9ac3f8
Joon Kang <jooneywork@gmail.com>
2021-12-01 14:51:31 -0400

    Initial Commit

---
+++ mathdown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mathdown
+//
+//  Created by Joon Kang on 12/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mathdown/mathdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  mathdownApp.swift
+//  mathdown
+//
+//  Created by Joon Kang on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct mathdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
